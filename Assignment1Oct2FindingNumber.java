aa964ee77d78114f756cfd99287af3c88e4d4ee4
Aswintester <55643261+Aswintester@users.noreply.github.com>
2021-10-03 00:48:05 +0530

    Add files via upload

---
+++ Assignment1Oct2FindingNumber.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Assignment1Oct2FindingNumber {
+
+	public static void main(String[] args) {
+		int num = 0;
+		if (num < 0) {
+			System.out.println(num + " is the negative number");
+		} else if (num < 0) {
+			System.out.println(num + " is the Positive number");
+		} else {
+			System.out.println(num + " is neither Positive or negative number");
+
+		}
+	}
+}
