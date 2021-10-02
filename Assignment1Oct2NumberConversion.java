b19d9109e04e76f96a03a82560f67932161cf00d
Aswintester <55643261+Aswintester@users.noreply.github.com>
2021-10-03 00:58:15 +0530

    Add files via upload

---
+++ Assignment1Oct2NumberConversion.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Assignment1Oct2NumberConversion {
+
+	public static void main(String[] args) {
+		int num=-40; //Initialize an integer with a negative number like, int number = -40;
+
+		if (num<0) //Check if the number is a negative number 
+		{
+			int num2 = (-1) *num; // If so, convert the number to a positive numer
+		System.out.println("The number " +num +" is converted to " +num2);
+		}
+
+}
+}
