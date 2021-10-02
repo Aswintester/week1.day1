99f25ed197feaed07ef0001826fb3e984677c100
Aswintester <55643261+Aswintester@users.noreply.github.com>
2021-10-03 00:10:31 +0530

    Update Assignment1Oct2PrimeNumber.java

--- Assignment1Oct2PrimeNumber.java
+++ Assignment1Oct2PrimeNumber.java
@@ -4,27 +4,35 @@ public class Assignment1Oct2PrimeNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num = 15;
-		//int reminder;
-		boolean flag = false;
+		int num = 15;   // Declare an int Input and assign a value 
+	
+		boolean flag = false; // Declare a boolean variable flag as false
 		
-		for (int i=2; i<=num/2; i++)
+		for (int i=2; i<=num/2; i++) // Iterate from 2 to half of the input
 		{
-			int reminder=num%i;
+			int reminder=num%i; // Divide the input with each for loop variable and check the remainder
 			
 			if (reminder==0) {
-				flag=true;
-				break;
+				flag=true;  // Set the flag as true when there is no remainder
+				break; // break the iterator
 			}	
 		}
 		
-		if (flag) {
-			System.out.println(num + " is not a prime number.");
+		if (flag) // Check the flag (Provide a condition)
+		{
+			System.out.println(num + " is not a prime number."); // Print 'Not a Prime' when the condition doesn't match
 		}
 		else {
-			System.out.println(num + " is a prime number.");
+			System.out.println(num + " is a prime number."); // Print 'Prime' when the condition matches
 		}
 
 	}
 
 }
+
+
+//What are my learnings from this code?
+	 //* 1) what is Prime Number
+	 //* 2) for loop, if else condition, Break condition
+	 //* 3) how to understand the logic using Pseudocode
+	
